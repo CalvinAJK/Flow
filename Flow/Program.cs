363c83b3639eadc05f0ca4544e918a4f51160595
CalvinAJK <calvinaujiakyn@gmail.com>
2024-02-04 12:53:02 +0000

    Added Auth0 configuration files

--- Flow/Program.cs
+++ Flow/Program.cs
@@ -1,6 +1,14 @@
+using Auth0.AspNetCore.Authentication;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddAuth0WebAppAuthentication(options =>
+{
+    options.Domain = builder.Configuration["Auth0:Domain"];
+    options.ClientId = builder.Configuration["Auth0:ClientId"];
+});
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -18,6 +26,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(

92e3b90695de5054cfa77105aacb3589fdb04823
CalvinAJK <calvinaujiakyn@gmail.com>
2024-02-05 17:10:26 +0000

    Added login, logout, profile page (Auth0)

---
+++ Flow/Models/ProfileViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Flow.Models
+{
+    public class ProfileViewModel
+    {
+        public string ProfileImage { get; set; }
+        public string Name { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}

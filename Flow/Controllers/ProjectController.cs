042aa34f3efec25902fb2de1ef7ab4ce35dddf5b
CalvinAJK <calvinaujiakyn@gmail.com>
2024-03-06 19:02:32 +0000

    Added Project Index and ProjectController

---
+++ Flow/Controllers/ProjectController.cs
@@ -0,0 +1,93 @@
+﻿using Flow.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Flow.Controllers
+{
+    public class ProjectController : Controller
+    {
+        private readonly FlowContext _context;
+
+        public ProjectController(FlowContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ProjectController
+        public async Task<IActionResult> Index()
+        {
+            var projects = await _context.Projects.ToListAsync();
+            return View(projects);
+        }
+
+        // GET: ProjectController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ProjectController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProjectController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProjectController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ProjectController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProjectController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ProjectController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}

0b90daf60beffc356bb33a16f4c30fb9da85e157
CalvinAJK <calvinaujiakyn@gmail.com>
2024-03-08 20:28:04 +0000

    Added modal for creating task, non functional

--- Flow/wwwroot/js/site.js
+++ Flow/wwwroot/js/site.js
@@ -2,3 +2,28 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+// Open task creation modal
+$('#addTaskBtn').click(function () {
+    $('#taskModal').modal('show');
+});
+
+// Handle task submission
+$('#saveTaskBtn').click(function () {
+    // Perform task creation form submission via AJAX or other method
+    // Upon successful submission, close the modal
+    $('#taskModal').modal('hide');
+});
+
+$('#hideTaskBtn').click(function () {
+    // Perform task creation form submission via AJAX or other method
+    // Upon successful submission, close the modal
+    $('#taskModal').modal('hide');
+});
+
+$('#CloseBtn').click(function () {
+    // Perform task creation form submission via AJAX or other method
+    // Upon successful submission, close the modal
+    $('#taskModal').modal('hide');
+});
